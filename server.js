9801cd6f4e68a8a0ed461b11a3c7b3cd505271ff
kaeriv93 <vsan93@yahoo.com>
2022-06-03 13:08:27 -0500

    added and connected to mongodb

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+// Dependencies
 require('dotenv').config()
 const express = require('express')
-// const {PORT = 4000, MONGODB_URL} = process.env
+const { default: mongoose } = require('mongoose')
+const {PORT = 4000, MONGODB_URL} = process.env
 const app = express()
+
+
+//Middleware
+const cors = require('cors')
+const morgan = require('morgan')
+
+//Mongo Connections
+mongoose.connect(MONGODB_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log('Mongoose DB connection successful! We are connected woot woot! 🔌 ⚡️ 🔌 ⚡️ 🔌 ⚡️ ')
+}).catch(err=>{
+    console.log(err.message)
+})
+
+//Using Middleware
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
     res.send('Hello World')
 })
-app.listen(4000, ()=>console.log(`We listening to port 4000`))
+app.listen(PORT, ()=>console.log(`We listening to port ${PORT}`))
