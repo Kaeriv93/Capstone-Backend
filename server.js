16e366d308af74e404d7fd67571a46a821a19165
kaeriv93 <vsan93@yahoo.com>
2022-06-07 00:27:08 -0500

    cleaned up routes and put users in a user route

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const { default: mongoose } = require('mongoose')
 const {PORT = 4000, MONGODB_URL} = process.env
 const app = express()
 const controllers = require('./Controllers')
+const userRoute = require('./routes/users')
 
 
 //Middleware
@@ -30,48 +31,11 @@ app.use(cors())
 const db = require('./models')
 
 //Users
-app.get('/users', async(req,res)=>{
-    try{
-        res.json(await db.User.find({}))
-    }catch(error){
-        res.status(400).json(error)
-    }
-})
 
-app.post('/users', async(req,res)=>{
-    try{
-        res.json(await db.User.create(req.body))
-    }catch(error){
-        res.status(400).json(error)
-    }
-})
-
-app.get('/userpage/:id', async(req,res)=>{
-    try{
-        res.json(await db.User.findById(req.params.id))
-    }catch(error){
-        res.status(400).json(error)
-    }
-})
-
-app.put('/userpage/:id', async(req,res)=>{
-    try{
-        res.json(await db.User.findByIdAndUpdate(req.params.id,req.body))
-    }catch(error){
-        res.status(400).json(error)
-    }
-})
-
-app.delete('/userpage/:id', async(req,res)=>{
-    try{
-        res.json(await db.User.findByIdAndRemove(req.params.id))
-    }catch(error){
-        res.status(400).json(error)
-    }
-})
 
 // Controllers Use
-app.use('/user', controllers.blog)
+// app.use('/user', controllers.blog)
+app.use('/', userRoute)
 
 //Get Home Route Test
 app.get('/',(req,res)=>{
