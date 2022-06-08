3b32d768c69c16235225577232b8953509a5bc98
kaeriv93 <vsan93@yahoo.com>
2022-06-08 08:36:16 -0500

    FULL CRUD for post

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema({
-    user:{
-        type: mongoose.Types.ObjectId,
-        ref:'User'
-    },
+    // user:{
+    //     type: mongoose.Types.ObjectId,
+    //     ref:'User'
+    // },
     content:{
         type:String
     },
