f4cc475ca4183e8c4f81e61f2de1305bd544898b
kaeriv93 <vsan93@yahoo.com>
2022-06-10 00:13:53 -0500

    added a default to post schema

--- models/Post.js
+++ models/Post.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema({
     userId:{
         type: String,
+        default:'629f8d7dd3bdfaf56f4259c0',
         required: true
     },
     content:{
