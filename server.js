c5a7a864c8ad4fba42d94fb5daa40ead80180c45
kaeriv93 <vsan93@yahoo.com>
2022-06-11 11:55:13 -0500

    fixing cors pt4

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-      origin: '*',
+      origin: [process.env.ORIGIN, 'http://localhost:3000'],
       credentials: true,
     })
   );
