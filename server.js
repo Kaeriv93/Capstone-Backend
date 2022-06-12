cc43782fcc0835472046907ae3aa1e20834872e7
kaeriv93 <vsan93@yahoo.com>
2022-06-12 10:24:12 -0500

    testing with local host

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
     cors({
-      origin: [process.env.ORIGIN, 'http://localhost:3000'],
+      origin: [process.env.ORIGIN || 'http://localhost:3000'],
       credentials: true,
     })
   );
