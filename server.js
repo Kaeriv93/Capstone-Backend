d8ebe4e0af66bf0df0efe6fa95e8217fac18eae3
kaeriv93 <vsan93@yahoo.com>
2022-06-12 01:56:18 -0500

    fix cookies netlify with heroku

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.use(
 
   const cookieOptions = {
     httpOnly: false,
-    secure: true,
-    sameSite: 'none'
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? 'none' : 'lax',
     }
     
     
