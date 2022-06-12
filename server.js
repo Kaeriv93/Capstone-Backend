67ca89a150ccfe3a6e43ddd48cd668d78b4762a1
kaeriv93 <vsan93@yahoo.com>
2022-06-12 02:10:00 -0500

    I broke something, undoing changes hopefully

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ mongoose.connect(MONGODB_URL,{
 //Using Middleware
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cookieParser())
+app.use(cookieParser(cookieOptions))
 app.use(
     cors({
       origin: [process.env.ORIGIN, 'http://localhost:3000'],
@@ -38,7 +38,12 @@ app.use(
     })
   );
 
-
+  const cookieOptions = {
+    httpOnly: false,
+    secure: true,
+    sameSite: 'none'
+    }
+    
     
 
 // Controllers Use
